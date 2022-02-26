c35a1dd1b5717a6a297a98a22f88773e9ac54d3c
kkkkiyo <k0121.sato@gmail.com>
2022-02-26 21:28:38 +0900

    パスワードに対するバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,9 @@ class User < ApplicationRecord
   validates :birth_date, presence: true
 
   with_options presence: true do
-    # ひらがな、カタカナ、漢字のみ許可する
-    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    # カタカナのみ許可する
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }    
     validates :last_name_kana, :first_name_kana,
               format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    # 半角英字数字のみ許可する
-    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: 'is invalid. Input half-width characters.' }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid. Input half-width characters.' }
   end
 end
