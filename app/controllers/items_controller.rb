d205d57ec595b404dae690e43dba5f19b7a6e469
kkkkiyo <k0121.sato@gmail.com>
2022-03-09 21:09:30 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
   def index  # indexアクションを定義した
+    def message_params
+      params.require(:item).permit(:image).merge(user_id: current_user.id)
+    end
   end
 end
