b11f2f19d9f854011f6f4076fac7550043669190
kkkkiyo <k0121.sato@gmail.com>
2022-06-27 17:01:30 +0900

    商品の一覧表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_fee_status
 
   validates :item_name, presence: true
   validates :item_info, presence: true
--- app/models/shipping_fee_status_id.rb
+++ app/models/shipping_fee_status_id.rb
@@ -1,4 +1,4 @@
-class Fee < ActiveHash::Base
+class ShippingFeeStatus < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },  
     { id: 2, name: '着払い(購入者負担)' },
@@ -6,5 +6,5 @@ class Fee < ActiveHash::Base
   ]
 
   include ActiveHash::Associations
-  has_many :articles
+  has_many :items
   end
