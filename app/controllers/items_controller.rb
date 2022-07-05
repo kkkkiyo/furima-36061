7bbfbc37b2e68660b72f1e767d4073b5852e490a
kkkkiyo <k0121.sato@gmail.com>
2022-07-05 21:36:10 +0900

    destroyアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,15 @@ end
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def item_params
