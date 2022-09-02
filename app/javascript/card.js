e6241a19604d02c31b2ac969e687b12bb598ba11
kkkkiyo <k0121.sato@gmail.com>
2022-09-02 19:00:35 +0900

    カード情報の各名称の名付け

---
+++ app/javascript/card.js
@@ -0,0 +1,19 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_0bccbafb7e56e6137926179e"); 
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
+    };
+  });
+};
+
+window.addEventListener("load", pay);
