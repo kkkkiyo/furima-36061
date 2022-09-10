57a0efa5cb922449c8889b29810a3b56bf1225b8
kkkkiyo <k0121.sato@gmail.com>
2022-09-10 20:53:04 +0900

    売却済みと出品者の条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,19 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
   
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    
+    unless current_user == @item.user
+    else
+      redirect_to root_path
+    end
+
+    unless @item.order.present?
+    else
+      redirect_to root_path
+    end
   end
 
   def new
